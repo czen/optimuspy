aa07423f92f3a38871f5933392b98f5cb0d61fa7
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2023-04-17 18:25:35 +0300

    Main page

--- optimuspy/settings.py
+++ optimuspy/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
     'web',
 ]
 
@@ -119,6 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = 'images/'
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
