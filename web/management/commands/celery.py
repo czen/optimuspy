e5ff44fbbbdd2f1e24bff51348791b2856f226e7
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2023-04-21 23:55:54 +0300

    Fixed a bug in rmtasks

--- web/management/commands/celery.py
+++ web/management/commands/celery.py
@@ -6,10 +6,10 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = 'Start Celery'
 
-    # pylint: disable=no-member, not-callable, broad-exception-caught
+    # pylint: disable=no-member, broad-exception-caught
     def handle(self, *args, **options):
         try:
             self.stdout.write(self.style.SUCCESS('Starting celery...'))
-            system('celery -A optimuspy worker -l INFO -E --concurrency 1')
+            system('celery -A optimuspy worker --concurrency 1')
         except Exception as exc:
             self.stdout.write(self.style.ERROR(exc))
--- web/management/commands/rmtasks.py
+++ web/management/commands/rmtasks.py
@@ -7,11 +7,11 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = 'Remove all task directories'
 
-    # pylint: disable=no-member, not-callable, broad-exception-caught
+    # pylint: disable=no-member, broad-exception-caught
     def handle(self, *args, **options):
         try:
             for p in settings.TASKS_PATH.iterdir():
-                self.stdout.write(self.style(f'Deleting {p}...'))
+                self.stdout.write(self.style.NOTICE(f'Deleting {p}...'))
                 rmtree(p)
             self.stdout.write(self.style.SUCCESS('Done'))
         except Exception as exc:
