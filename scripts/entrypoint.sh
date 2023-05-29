c5d065ac3f14301a6ad792f5fa75e931f9ebdfeb
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2023-05-29 15:21:36 +0300

    Updated entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,4 +4,4 @@
 python3 manage.py wait_for_db &&
 python3 manage.py migrate
 celery -A optimuspy worker --concurrency 1 -P solo --loglevel=info &
-gunicorn optimuspy.wsgi --bind 0.0.0.0:8000
+daphne -b 0.0.0.0 -p 8000 optimuspy.asgi:application
